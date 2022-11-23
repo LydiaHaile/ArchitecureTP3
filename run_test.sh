8b06d2f508d3bf6e09c1a63fc3008565a80f9da6
LydiaHaile <lydiaefrem@gmail.com>
2022-11-23 16:42:06 -0500

    updated run_test.sh to include redis

--- run_test.sh
+++ run_test.sh
@@ -45,15 +45,15 @@ cd YCSB/ycsb-0.17.0
 # run mongodb three times on workload a - save output in directory results/mongodb/workload_A
 for iter in {1..3}
     do
-    ./bin/ycsb.sh load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_A/output_load${iter}
-    ./bin/ycsb.sh run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_A/output_run${iter}
+        ./bin/ycsb.sh load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_A/output_load${iter}
+        ./bin/ycsb.sh run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_A/output_run${iter}
     done
 
 # run mongodb three times on workload b - save output in directory results/mongodb/workload_B
 for iter in {1..3}
     do
-    ./bin/ycsb.sh load mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_B/output_load${iter}
-    ./bin/ycsb.sh run mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_B/output_run${iter}
+        ./bin/ycsb.sh load mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_B/output_load${iter}
+        ./bin/ycsb.sh run mongodb -s -P workloads/workloadb -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?w=1 > ../../results/mongodb/workload_B/output_run${iter}
     done
 
 # stops mongodb docker containers
@@ -62,3 +62,19 @@ docker stop $(docker ps -a -q)
 # removes mongodb docker network
 docker network rm mongoCluster
  
+# -------------------------
+# -------- REDIS ----------
+# -------------------------
+
+cd targetDB/data
+
+# start services defined in docker-compose-redis.yml
+docker-compose -f docker-compose-redis.yml up -d 
+
+cd ../../YCSB/ycsb-0.17.0
+ 
+# loads redis three times on workload a 
+for iter in {1..3}
+    do
+        bin/ycsb.sh load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p redis.cluster=true
+    done
